feb192adcb2403efb15dc1d164908c9617a75e5f
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-07-11 17:07:05 -0300

    dtos response e request professor

---
+++ src/data/api_response.go
@@ -0,0 +1,6 @@
+package data
+
+type ResponseApi struct {
+	Code   int    `json:"codigo"`
+	Status string `json:"status`
+}
---
+++ src/data/professor/professor_response.go
@@ -0,0 +1,6 @@
+package professor
+
+type ProfessorResponse struct {
+	Id   int    `json:"id"`
+	Nome string `json:"nome"`
+}
