0c50f8a0da161df9c5ad6c4e81e64a28bad5e85b
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-08-25 21:43:32 -0300

    att

--- src/data/professor_response.go
+++ src/data/professor_response.go
@@ -1,6 +1,7 @@
 package data
 
 type ProfessorResponse struct {
-	Id   uint   `json:"id"`
-	Nome string `json:"nome"`
+	Id    uint   `json:"id"`
+	Nome  string `json:"nome"`
+	Email string `json: "email"`
 }
