61e54211c5f1661261e68f6177d1706156513a9d
Gabrielcamargos28 <gabrielcamargosptc@gmail.com>
2024-08-28 18:38:23 -0300

    corrigido delet

--- src/models/turma.go
+++ src/models/turma.go
@@ -6,9 +6,9 @@ type Turma struct {
 	Semestre    string      `gorm:"type:varchar(50);not null"`
 	Ano         int         `gorm:"not null"`
 	ProfessorId uint        `gorm:"index"`
-	Professor   Professor   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
-	Atividades  []Atividade `gorm:"foreignKey:TurmaId;constraint:OnDelete:CASCADE;"`
-	Alunos      []Aluno     `gorm:"many2many:turma_alunos;constraint:OnDelete:CASCADE;"`
+	Professor   Professor   `gorm:"constraint:OnUpdate;"`
+	Atividades  []Atividade `gorm:"foreignKey:TurmaId;"`
+	Alunos      []Aluno     `gorm:"many2many:turma_alunos;"`
 }
 
 func (Turma) TableName() string {
